7526ae4c88f4f0bc6a84b135196fe61da33afdb9
hideo55 <hide.o.j55@gmail.com>
2011-11-19 18:21:48 +0900

    initial commit

---
+++ lib/FormValidator/Lite/Constraint/Mouse.pm
@@ -0,0 +1,32 @@
+package FormValidator::Lite::Constraint::Mouse;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+FormValidator::Lite::Constraint::Mouse -
+
+=head1 SYNOPSIS
+
+  use FormValidator::Lite::Constraint::Mouse;
+
+=head1 DESCRIPTION
+
+FormValidator::Lite::Constraint::Mouse is
+
+=head1 AUTHOR
+
+Hideaki Ohno E<lt>hide.o.j55 {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
