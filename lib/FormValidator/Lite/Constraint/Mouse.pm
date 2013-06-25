4af6040503e4f7eddc5be07edce62088620900ed
hideo55 <hide.o.j55@gmail.com>
2013-06-26 08:11:00 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index ed4c386..93221e8 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension FormValidator::Lite::Constraint::Mouse
    
     {{$NEXT}}
    
    +0.04 2013-06-25T23:10:54Z
    +    - Switch to Minilla
    +
     0.03    Sun Apr 28 03:40:00 2013
             - Change reposiroty informarion

--- lib/FormValidator/Lite/Constraint/Mouse.pm
+++ lib/FormValidator/Lite/Constraint/Mouse.pm
@@ -4,7 +4,7 @@ use warnings;
 use FormValidator::Lite::Constraint;
 use Mouse::Util::TypeConstraints ();
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 my $get_constraint = \&Mouse::Util::TypeConstraints::find_type_constraint;
 
