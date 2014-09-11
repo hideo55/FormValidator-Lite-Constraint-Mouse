5271d18a2ad2f0ac1b0b8caed31a186549ba827f
hideo55 <hide.o.j55@gmail.com>
2014-09-12 02:54:25 +0900

    Checking in changes prior to tagging of version 0.05.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 93221e8..3c4d4b0 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension FormValidator::Lite::Constraint::Mouse
    
     {{$NEXT}}
    
    +0.05 2014-09-11T17:54:20Z
    +    - Fixed violation of CPAN Meta Spec
    +
     0.04 2013-06-25T23:10:54Z
         - Switch to Minilla

--- lib/FormValidator/Lite/Constraint/Mouse.pm
+++ lib/FormValidator/Lite/Constraint/Mouse.pm
@@ -4,7 +4,7 @@ use warnings;
 use FormValidator::Lite::Constraint;
 use Mouse::Util::TypeConstraints ();
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 my $get_constraint = \&Mouse::Util::TypeConstraints::find_type_constraint;
 
